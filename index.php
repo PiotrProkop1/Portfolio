79d54cb6911985ad77c11b07f7aaec8d38e12808
piotr <piotr@Exacts-MacBook-Pro.local>
2018-12-13 14:29:02 +0100

    Added improved about section...

--- index.php
+++ index.php
@@ -11,13 +11,25 @@
     <header id="header">
         <img src="static/header-bg.jpg" alt="header background." class="header-img">
         <div class="header-bg-div"></div>
-        <span class="header-info">Wanna a website?</span>
-        <button type="button" class="header-btn" id="header-btn">Let's Begin!</button>
+        <span class="header-info">Piotr Prokop Portfolio</span>
+        <button type="button" class="header-btn" id="header-btn">Continue...</button>
     </header>
 
     <main>
         <div class="about">
-            
+            <img src="static/my-img.jpg" alt="My Img." class="about-img">
+
+            <h3>About me:</h3>
+            <p class="about-text">
+               Hi, my name is Piotr and i'm looking for programming job.
+               I don't have any commercial experience yet, but i have some decent programming skill
+               and i always want to improve.
+             </p>
+
+             <p class="about-text">
+               I'm intersted in working on Javascript and some php, but other web technologies like
+               React or Node are also fine.
+             </p>
         </div>
 
         <div class="skills">
